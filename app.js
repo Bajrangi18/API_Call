a25c7a333fbdce009abe895fdf54f3813527b086
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-10-04 21:22:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,23 +2,18 @@
 document.getElementById('tap').addEventListener("click", updateFunc)
 
 function updateFunc() {
-    const url = 'https://5961vq2tph.execute-api.us-east-1.amazonaws.com/Stage2/'
-        const customHeaders = {
-            "Access-Control-Allow-Origin": "https://5961vq2tph.execute-api.us-east-1.amazonaws.com",
-            "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization",
-            "Content-Type": "application/json",
-            "x-api-key": "gbELimwuDq9JOvIrtaUZFfVzQTBl40788a7dJLe0"
+    fetch('https://5961vq2tph.execute-api.us-east-1.amazonaws.com/Stage2/',{
+        mode: 'cors',
+        headers: {
+          'x-api-key': "gbELimwuDq9JOvIrtaUZFfVzQTBl40788a7dJLe0"
         }
-     
-        fetch(url, {
-            method: "GET",
-            mode: 'cors',
-            headers: customHeaders
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-            });
-    document.getElementById('lenVal').innerHTML = "1"
+    })
+    .then(res => res.json())
+    .then((out) => {
+    // console.log(out['Items'][0])
+    let obj = out['Items']
+    document.getElementById('lenVal').innerHTML = obj.length
+    })
+    .catch(err => console.error(err));
+    // document.getElementById('lenVal').innerHTML = 
 }
