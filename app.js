78eb3be9485a69a0968c356095e403ebc0a013cb
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-10-04 20:22:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,23 @@
 document.getElementById('tap').addEventListener("click", updateFunc)
 
 function updateFunc() {
-    $(document).ready(function() {
-        $.ajax({
-          url: "https://5961vq2tph.execute-api.us-east-1.amazonaws.com/Stage2/",
-          type: "GET",
-          headers: {"x-api-key": "gbELimwuDq9JOvIrtaUZFfVzQTBl40788a7dJLe0"},
-          success: function(result) {
-            console.log(result);
-          },
-          error: function(error) {
-            console.log(error);
-          }
-        });
-      });
+    const url = 'https://5961vq2tph.execute-api.us-east-1.amazonaws.com/Stage2/'
+        const customHeaders = {
+            "Access-Control-Allow-Origin": "https://5961vq2tph.execute-api.us-east-1.amazonaws.com",
+            "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization",
+            "Content-Type": "application/json",
+            "x-api-key": "gbELimwuDq9JOvIrtaUZFfVzQTBl40788a7dJLe0"
+        }
+     
+        fetch(url, {
+            method: "GET",
+            mode: 'cors',
+            headers: customHeaders
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            });
     document.getElementById('lenVal').innerHTML = "1"
 }
